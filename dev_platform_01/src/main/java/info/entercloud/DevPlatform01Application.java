18dea1c42c697b31a01efb99c8f16bff10134aac
Andrew Thompson <andrew@Andrews-MacBook-Pro.local>
2017-03-05 01:37:28 -0500

    spring boot application initial

---
+++ dev_platform_01/src/main/java/info/entercloud/DevPlatform01Application.java
@@ -0,0 +1,12 @@
+package info.entercloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPlatform01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPlatform01Application.class, args);
+	}
+}
