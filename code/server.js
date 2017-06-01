ae7790235c911004d3cb5b23493adbde8a484af3
Andrew Thompson <admin@entercloud.info>
2017-05-31 22:29:34 -0400

    Update server.js

--- code/server.js
+++ code/server.js
@@ -1,16 +1,8 @@
-'use strict';
-
 const Path = require('path');
 const Hapi = require('hapi');
 const Inert = require('inert');
-//const Path = require('path'); 
-//const Inert = require('inert');
-//const Boom = require('boom');
-//const glob = require('glob');
-//const secret = require('./config');
-//const mongoose = require('mongoose');
 
-const server = new Hapi.Server() ({
+const server = new Hapi.Server({
     connections: {
         routes: {
             files: {
@@ -18,74 +10,28 @@ const server = new Hapi.Server() ({
             }
         }
     }
-  });
+});
+server.connection({ port: 3000 });
 
-// const dbUrl = 'mongodb://0.0.0.0:27017/hapi-app'; 
-//server.register(reguire('hapi-auth-jwt'), (err) => { 
+server.register(Inert, () => {});
 
-// strategy both a name 
-// scheme of 'jwt' 
-//server.auth.strategy('jwt', 'jwt', { 
-//   key: secret, 
-//   verifyOptions: { algorithms: ['HS256'] }
-// });  --
-
-// look through the routes in subdirectories of api and create new route for each
-//glob.sync('api/**/routes/*.js', {
-//  root: _dirname
-// }).forEach(file => { 
-//  const route = require(path.join(_dirname, file)); 
-//  server.route(route);
-//  });
-// }); 
-server.connection({ port: 3000, host: '0.0.0.0' });
-server.register('inert'), ()=>{};
-                
 server.route({
-        method: 'GET',
-        path: '/{param*}',
-        handler: 
-    directory: {
-            path: '/var/www/html',
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: '.',
             redirectToSlash: true,
             index: true
-                function (request, reply) {reply.file('index.html');
-                                          }
-)};
-        
-       // method: 'GET',
-        //path: '/start',
-       // handler: function (request, reply) {
-       //     reply.file('/public/entercloud/entercloud_landing/index.html'); 
-       // }
-    
-       // method: 'GET',
-       // path: '/dashboard',
-       // handler: function (request, reply) {
-       //     reply.file('/public/entercloud/entercloud_dashboard/index.html'); 
-       // }
-                                            
-      
-       // method: 'GET',
-       // path: '/{param*}',
-       // handler: {
-       //   directory: {
-       //     path: 'public'
-      //v  }
-    //}
-                                            
-    });
+        }
+    }
 });
-
-
 
 server.start((err) => {
 
     if (err) {
         throw err;
     }
- // once started, connect to mongo through mongoose
-    //mongoose.connect(dbUrl, {}, (err) => { 
-    console.log(`server on gofundme.com/entercloud
-    	${server.info.uri}`);
+
+    console.log('Server running at:', server.info.uri);
 });
